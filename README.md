a8fdadee5638f72443298a3f1d8bcd9f2f8f1a0c
Christian Stigen Larsen <csl@csl.name>
2017-09-08 21:03:53 +0200

    Adds monoalphabetic breaker

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-Private repo for Christian Stigen Larsen
+Coursework for DAT-500
+======================
+
+Private repository for Christian Stigen Larsen.
+
+
+References
+----------
+
+  * (Cornell Cryptography)[https://www.math.cornell.edu/~mec/2003-2004/cryptography/subs/hints.html}
--- util.py
+++ util.py
@@ -1,3 +1,5 @@
+import collections
+
 def normalize(s):
     s = s.replace(" ", "")
     s = s.replace("\n", "")
@@ -5,9 +7,27 @@ def normalize(s):
     s = s.strip().upper()
     return s
 
+def reverse_pairs(pairs):
+    for a, b in pairs:
+        yield (b, a)
+
 def encrypt(key, text):
     pass
 
 def read_file(filename="cipher.txt"):
     with open(filename, "rt") as f:
         return normalize(f.read())
+
+def relative_freqs(text):
+    frequencies = collections.Counter(text)
+
+    for character, count in frequencies.items():
+        yield (float(count) / len(text), character)
+
+def split_string(string, length, start=0, tail=True):
+    while start + length < len(string):
+        yield string[start:start+length]
+        start += length + 1
+
+    if tail:
+        yield string[start:]
