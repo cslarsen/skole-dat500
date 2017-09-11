dd4444972ee47969f223aecd98dbcf2869253f34
Christian Stigen Larsen <csl@csl.name>
2017-09-11 23:10:48 +0200

    More tests

--- compare.py
+++ compare.py
@@ -6,6 +6,34 @@ Python version.
 import csdes
 import sdes
 
+for n in range(2**10):
+    py = sdes.p10(n)
+    cpp = csdes.p10(n)
+    if cpp != py:
+        raise RuntimeError("p10(0x%x) py=0x%x cpp=0x%x" % (n, py, cpp))
+print("ok p10")
+
+for n in range(2**10):
+    py = sdes.p8(n)
+    cpp = csdes.p8(n)
+    if cpp != py:
+        raise RuntimeError("p8(0x%x) py=0x%x cpp=0x%x" % (n, py, cpp))
+print("ok p8")
+
+for n in range(2**4):
+    py = sdes.p4(n)
+    cpp = csdes.p4(n)
+    if cpp != py:
+        raise RuntimeError("p4(0x%x) py=0x%x cpp=0x%x" % (n, py, cpp))
+print("ok p4")
+
+for n in range(2**8):
+    py = sdes.ip(n)
+    cpp = csdes.ip(n)
+    if cpp != py:
+        raise RuntimeError("ip(0x%x) py=0x%x cpp=0x%x" % (n, py, cpp))
+print("ok ip")
+
 # Check by converting C++ to tuple
 for key in range(2**10):
     py = sdes.create_subkeys(key)
@@ -18,6 +46,7 @@ for key in range(2**10):
         raise RuntimeError(
             "create_subkeys(key=0x%x) py=%r cpp=%r" % (key,
                 py, cpp))
+print("ok create_subkeys")
 
 # Check by converting Python to 20-bit value
 for key in range(2**10):
