40ea01859b8d070f8b27b92fd442a0d3438bfddf
Christian Stigen Larsen <csl@csl.name>
2017-09-13 08:52:18 +0200

    Adds support for SDES cracking

--- bruteforce.py
+++ bruteforce.py
@@ -28,6 +28,7 @@ def read_ciphertext(filename):
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
+
     p.add_argument("FILE", type=str, nargs="?", default="ctx2.txt",
             help="""Ciphertext file to break, consiting of 0 and 1 in ASCII.""")
     p.add_argument("--start", type=int, default=32,
@@ -39,31 +40,44 @@ visible ASCII range of 32 to 126.""".lstrip())
     p.add_argument("--stop", type=int, default=126,
             help="""End of range; see --start""")
 
+    p.add_argument("--sdes", default=False, action="store_true",
+            help="""Bruteforces SDES key instead of TripleSDES.""")
+
     opts = p.parse_args()
+
+    if opts.sdes:
+        print("File is SDES-encrypted")
+        bruteforce_key = csdes.bruteforce_sdes_key
+    else:
+        print("File is TripleSDES-encrypted")
+        bruteforce_key = csdes.bruteforce_3sdes_key
 
     ciphertext = read_ciphertext(opts.FILE)
 
     # Bruteforce it
     start = mark_time()
-    bf = csdes.bruteforce_3sdes_key(ciphertext, opts.start, opts.stop)
+    bf = bruteforce_key(ciphertext, opts.start, opts.stop)
     stop = mark_time()
 
     print("Found %d keys in %.1f ms CPU time" % (bf.count, 1000.0*(stop - start)))
 
-    k1 = (bf.key & 0xffc00) >> 10;
-    k2 = (bf.key & 0x003ff);
-
     print("First found key:")
     print("  20-bit key: 0x%5.5x" % bf.key);
-    print("  10-bit k1:    0x%3.3x" % k1);
-    print("  10-bit k2:    0x%3.3x" % k2);
-
     print("  key binary: %20s" % bin(bf.key))
-    print("  k1 binary:  %10s.........." % bin(k1))
-    print("  k2 binary:  ..........%12s" % bin(k2))
+
+    if not opts.sdes:
+        k1 = (bf.key & 0xffc00) >> 10;
+        k2 = (bf.key & 0x003ff);
+        print("  10-bit k1:    0x%3.3x" % k1);
+        print("  10-bit k2:    0x%3.3x" % k2);
+        print("  k1 binary:  %10s.........." % bin(k1))
+        print("  k2 binary:  ..........%12s" % bin(k2))
 
     if bf.count == 1:
-        plaintext = csdes.triplesdes_decrypt_buffer(k1, k2, ciphertext)
+        if opts.sdes:
+            plaintext = csdes.sdes_decrypt_buffer(bf.key, ciphertext)
+        else:
+            plaintext = csdes.triplesdes_decrypt_buffer(k1, k2, ciphertext)
         print("Plaintext (%d bytes):" % len(plaintext))
         print("%r" % plaintext)
     else:
