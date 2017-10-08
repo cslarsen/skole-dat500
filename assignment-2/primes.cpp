33dd60e94022aa7ca45b240e31b13fc1349528f4
Christian Stigen Larsen <csl@csl.name>
2017-10-08 23:24:02 +0200

    rewrites primes

--- assignment-2/primes.cpp
+++ assignment-2/primes.cpp
@@ -1,5 +1,7 @@
-// Prime number generator
+// Prime number generator using GMP
+//
 // Written by Christian Stigen
+// 2017 DAT-510 UiS
 //
 // To compile, you need GNU MP (GMP), and you need to link with it:
 // g++ primes.cpp -oprimes -I/.../include -L/.../lib -lgmp -lgmpxx
@@ -8,14 +10,18 @@
 #include <iostream>
 #include <math.h>
 #include <stdio.h>
+#include <time.h>
 
+// Random number generator. Using a single one means that the whole code is not
+// thread-safe.
 static gmp_randclass *rnd = NULL;
 
-// Calculates a^x mod n
-mpz_class pow_mod(mpz_class a, mpz_class x, const mpz_class& n)
+// Fast calculation of a^x mod n
+//
+// This algorithm is taken from Bruce Schneier's book "Applied Cryptography".
+// See http://en.wikipedia.org/wiki/Modular_exponentiation
+static mpz_class pow_mod(mpz_class a, mpz_class x, const mpz_class& n)
 {
-  // This algorithm is taken from Bruce Schneier's book "Applied Cryptography".
-  // See http://en.wikipedia.org/wiki/Modular_exponentiation
   mpz_class r = 1;
 
   while (x > 0) {
@@ -29,61 +35,82 @@ mpz_class pow_mod(mpz_class a, mpz_class x, const mpz_class& n)
   return r;
 }
 
-// Sets the random seed.
-void initialize_seed(const size_t bytes)
+// Sets the initial random seed.
+//
+// Bytes is how many bytes from /dev/urandom to use as a seed. If set to zero,
+// will just seed with the current time.
+//
+// Returns the number of bytes read from /dev/urandom.
+//
+extern "C" int initialize_seed(const size_t bytes)
 {
   if (!rnd)
     rnd = new gmp_randclass(gmp_randinit_default);
 
-  FILE *f = fopen("/dev/urandom", "rb");
-  if (!f) {
-    perror("/dev/urandom");
-    exit(1);
-    return;
+  if (bytes > 0) {
+    FILE *f = fopen("/dev/urandom", "rb");
+    if (!f) {
+      perror("/dev/urandom");
+      // Fall-through to use current time as seed
+    } else {
+      mpz_class seed = 0;
+      for ( size_t i = 0; i < bytes; ++i ) {
+        int n = fgetc(f);
+        seed = (seed << 8) | n;
+      }
+
+      fclose(f);
+      rnd->seed(seed);
+      return bytes;
+    }
   }
 
-  mpz_class seed = 0;
-  for ( size_t i = 0; i < bytes; ++i ) {
-    int n = fgetc(f);
-    seed = (seed << 8) | n;
-  }
-
-  fclose(f);
-  rnd->seed(seed);
+  // Just seed with the current time
+  rnd->seed(time(NULL));
+  return 0;
 }
 
-mpz_class randint(const mpz_class& lowest, const mpz_class& highest)
+static mpz_class randint(const mpz_class& lowest, const mpz_class& highest)
 {
-  if (!rnd)
-    initialize_seed(256/8);
+  if (!rnd) {
+    // Default number of bytes to read for seed
+    initialize_seed(256 / 8);
+  }
+
   return rnd->get_z_range(highest - lowest) + lowest;
 }
 
-// Miller-Rabin primality test
-bool prob_prime(const mpz_class& n, const size_t rounds)
+// The Miller-Rabin primality test
+//
+// This part was closely based on an earlier version of the Miller-Rabin
+// primality tester that I wrote in 2012, available at:
+// https://github.com/cslarsen/miller-rabin
+//
+static bool prob_prime(const mpz_class& n, const size_t rounds)
 {
-
-  if (n <= 1)
+  // Base cases:
+  //   - Treat 1 and all negative numbers as non-prime
+  //   - Drop even numbers (including zero)
+  if (n <= 1 || (n & 1) == 0)
     return false;
 
+  // Do not iterate for the first two prime numbers
   if (n == 2 || n == 3)
     return true;
 
-  // Even numbers cannot be prime
-  if ((n & 1) == 0)
-    return false;
-
   // Write n-1 as d*2^s by factoring powers of 2 from n-1
   size_t s = 0;
-  mpz_class m = n - 1;
-  while ((m & 1) == 0) {
-    ++s;
-    m >>= 1;
+  {
+    mpz_class m = n - 1;
+    while ((m & 1) == 0) {
+      ++s;
+      m >>= 1;
+    }
   }
-  mpz_class d = (n - 1) / (mpz_class(1) << s);
+  const mpz_class d = (n - 1) / (mpz_class(1) << s);
 
   for (size_t i = 0; i < rounds; ++i) {
-    mpz_class a = randint(2, n - 2);
+    const mpz_class a = randint(2, n - 2);
     mpz_class x = pow_mod(a, d, n);
 
     if (x ==1 || x == (n - 1))
@@ -91,17 +118,21 @@ bool prob_prime(const mpz_class& n, const size_t rounds)
 
     for (size_t r = 0; r < (s-1); ++r) {
       x = pow_mod(x, 2, n);
-      if (x == 1)
+      if (x == 1) {
+        // Definitely not a prime
         return false;
+      }
       if (x == n - 1)
         break;
     }
 
-    if (x != (n - 1))
+    if (x != (n - 1)) {
+      // Definitely not a prime
       return false;
+    }
   }
 
-  // Probably prime
+  // Might be prime
   return true;
 }
 
@@ -135,8 +166,8 @@ int main()
       mpz_class p = 2*q + 1;
 
       if (prob_prime(p, accuracy)) {
-        std::cout << q << std::endl;
-        std::cout << p << std::endl;
+        std::cout << "q = " << q << std::endl;
+        std::cout << "p = " << p << std::endl;
         break;
       }
     }
