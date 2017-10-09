102d004440ee4bf7ac8590ba5af8caa656c76877
Christian Stigen Larsen <csl@csl.name>
2017-10-09 03:11:22 +0200

    more

--- assignment-2/example_dh_params.py
+++ assignment-2/example_dh_params.py
@@ -1,11 +1,31 @@
 from app import generate_dh_params, show
+import miller_rabin as mr
+import galois
 
 if __name__ == "__main__":
-    for bits in (8, 128, 256):
+    for bits in (8, 10, 16, 24, 128, 256):
         q, p, g = generate_dh_params(bits)
         print("Example %d-bit Diffie-Hellman global parameters:" % bits)
-        print("  q = %d" % q)
-        print("    = 0x%x" % q)
-        print("  p = %d" % p)
-        print("    = 0x%x" % p)
-        print("  g = %d" % g)
+        print("         q = %d" % q)
+        print("           = 0x%x" % q)
+        print("  p (2q+1) = %d" % p)
+        print("           = 0x%x" % p)
+
+        # find generator
+        found = False
+        for a in range(2, 10):
+            nums = set([n for n in range(q)])
+            try:
+                for x in range(1, q):
+                    num = mr.pow_mod(a, x, q)
+                    nums.remove(num)
+                if nums == set([0]):
+                    found = a
+                    break
+            except KeyError:
+                continue
+        if found:
+            print("   found g = %d" % found)
+        else:
+            print("   did not find g in given range")
+        print("")
