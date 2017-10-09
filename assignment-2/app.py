aa203c9ad4dd443a387d07d8be086ab57913d43e
Christian Stigen Larsen <csl@csl.name>
2017-10-09 06:43:14 +0200

    it works

--- assignment-2/app.py
+++ assignment-2/app.py
@@ -5,17 +5,113 @@ All code written by Christian Stigen
 """
 
 import argparse
-import hashlib # only used for SHA1 sums
+import json
 import math
 import random
+import socket
 import sys
+import threading
 
 # Local imports
 import bbs
-from galois import GF
+from galois import GF, IntMod
 import csdes
 import miller_rabin as mr
 import modp
+
+class Connection(object):
+    def __init__(self, q, p, bits, privkey, pubkey, port, remote_host=None):
+        self.q = q
+        self.p = p
+        self.bits = bits
+        self.privkey = privkey
+        self.pubkey = pubkey
+        self.port = port
+        self.remote_host = remote_host
+
+    def start_server(self):
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind(("localhost", self.port))
+        server.listen(1)
+        log("Waiting for connections on port %d\n" % self.port)
+
+        while True:
+            connection, address = server.accept()
+            self.exchange_keys(connection)
+            connection.close()
+        server.close()
+
+    def start_client(self):
+        log("Connecting to remote host %s:%d ... " % (self.remote_host,
+            self.port))
+        remote = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        remote.connect((self.remote_host, self.port))
+        log("\n")
+        self.exchange_keys(remote)
+
+    def prompt(self, prefix):
+        if sys.version_info.major <= 3:
+            return raw_input(prefix)
+        else:
+            return input(prefix)
+
+    def exchange_keys(self, remote, buflen=4096):
+        log("Sending our public key\n")
+        remote.send(json.dumps({"pubkey": int(self.pubkey)}))
+        print("")
+
+        log("Receiving remote public key\n")
+        reply = json.loads(remote.recv(buflen))
+        if "pubkey" in reply:
+            remote_pubkey = reply["pubkey"]
+            remote_pubkey = IntMod(remote_pubkey, self.pubkey.mod)
+
+            show("Remote pubkey", remote_pubkey)
+            print("")
+
+            log("Creating shared key\n")
+            Kab = create_shared_key(self.privkey, remote_pubkey)
+            show("Shared key Kab", Kab)
+            print("")
+
+            # Create a shared CSPRNG for use in communication.
+            # NOTE: Since this is a CHAT and not a one-off file transfer, we
+            # may QUICKLY get into syncing issues, as in the position in the
+            # PRNG may drift.
+            seed = int(Kab)
+            shared_prng = bbs.BlumBlumShub(seed, self.p, self.q, bits=10)
+
+            def encrypt(plaintext):
+                return encrypt_with_csprng(shared_prng, map(ord, plaintext))
+
+            def decrypt(ciphertext):
+                return decrypt_with_csprng(shared_prng, ciphertext)
+
+            # Start chat
+            self.chat(encrypt, decrypt, remote)
+
+    def chat(self, encrypt, decrypt, remote, buffer_length=4096):
+        log("*** Entering encrypted chat. Hit CTRL+D to exit ***\n\n")
+
+        def receive():
+            while True:
+                reply = remote.recv(buffer_length)
+                if len(reply) > 0:
+                    print("Encrypted reply: %r" % reply)
+                    reply = decrypt(reply)
+                    print("Decrypted reply: %s" % reply)
+
+        try:
+            # Start a background thread to receive
+            thread = threading.Thread(target=receive)
+            thread.start()
+
+            while True:
+                message = encrypt(self.prompt("You: "))
+                print("Encrypted message: %r" % message)
+                remote.send(message)
+        finally:
+            sys.exit(0)
 
 def is_prime(n, accuracy=None):
     if accuracy is None:
@@ -37,9 +133,8 @@ def generate_dh_params(bits):
     acc1 = mr.estimate_accuracy(bits-1)
     acc2 = mr.estimate_accuracy(bits)
 
-    # TODO: This is not entirely correct. At least it should be tested, albeit
-    # the space is too big to enumerate exhaustively. Need more efficient
-    # methods.
+    # TODO: THis is not correct, must use discrete logarithm approach to
+    # actually find it. See example_dh_params.py
     generator = 2
 
     while True:
@@ -65,16 +160,16 @@ def generate_keypair(prng, generator, p, q, priv=None):
     pub = g**priv
     return priv, pub
 
-def create_csprng(seed, bits):
-    return bbs.BlumBlumShub.create(prime_bits=bits, seed=int(seed))
+def create_csprng(q, p, seed, bits):
+    return bbs.BlumBlumShub.create(q=q, p=p, prime_bits=bits, seed=int(seed))
+    return bbs.BlumBlumSHub(int(seed), p, q)
 
 def encrypt(key, plaintext):
     k1 = (key & 0b11111111110000000000) >> 10
     k2 =  key & 0b1111111111
     return csdes.triplesdes_encrypt_buffer(k1, k2, plaintext)
 
-def encrypt_with_csprng(q, p, seed, plaintext):
-    prng = bbs.BlumBlumShub(seed=seed, bits=10, q=q, p=p)
+def encrypt_with_csprng(prng, plaintext):
     ciphertext = ""
     for plainbyte in plaintext:
         k1 = prng.next()
@@ -83,8 +178,7 @@ def encrypt_with_csprng(q, p, seed, plaintext):
         ciphertext = ciphertext + chr(cipherbyte)
     return ciphertext
 
-def decrypt_with_csprng(q, p, seed, ciphertext):
-    prng = bbs.BlumBlumShub(seed=seed, bits=10, q=q, p=p)
+def decrypt_with_csprng(prng, ciphertext):
     plaintext = ""
     for cipherbyte in map(ord, ciphertext):
         k1 = prng.next()
@@ -130,6 +224,12 @@ def parse_args():
             help="IKE cyclic group to use: %s (default 2048)" %
                 ", ".join(map(str, modp.groups.keys())))
 
+    p.add_argument("--port", type=int, default=45678,
+            help="Port number (default 45678)")
+
+    p.add_argument("--remote-host", type=str, default=None,
+            help="Address of remote host")
+
     opts = p.parse_args()
     return opts
 
@@ -172,16 +272,13 @@ def main(opts):
     csprng_key = bbs.BlumBlumShub.create(bits)
     log("\n")
 
-    log("Generating %d-bit keypair for Alice ... " % bits)
-    #priva, puba = generate_keypair(p, q, 312)
-    priva, puba = generate_keypair(csprng_key, generator, p, q)
-    log("\n")
-
-    log("Generating %d-bit keypair for Bob ... " % bits)
-    #privb, pubb = generate_keypair(p, q, 24)
-    privb, pubb = generate_keypair(csprng_key, generator, p, q)
-    log("\n")
-    log("\n")
+    print("Blum Blum Shub parameters for generating keys")
+    show("p", csprng_key.p)
+    print("")
+    show("q", csprng_key.q)
+    print("")
+    show("m (Blum number)", csprng_key.m)
+    print("")
 
     print("Global Diffie-Hellman (mod p) parameters")
     print("  g = %d (generator)" % generator)
@@ -193,75 +290,22 @@ def main(opts):
     print("  prime(p, %d): %s" % (acc, primep))
     print("")
 
-    print("Blum Blum Shub parameters")
-    show("p", csprng_key.p)
-    print("")
-    show("q", csprng_key.q)
-    print("")
-    show("m (Blum number)", csprng_key.m)
-    print("")
-
-    print("Alice's keys")
-    show("Alice privkey", priva)
-    print("")
-    show("Alice pubkey", puba)
-    print("")
-
-    print("Bob's keys")
-    show("Bob privkey", privb)
-    print("")
-    show("Bob pubkey", pubb)
-    print("")
-
-    #pubb = dh_exchange(bob, puba)
-
-    aliceKab = create_shared_key(priva, pubb)
-    bobKab = create_shared_key(privb, puba)
-    assert(aliceKab == bobKab)
-    Kab = aliceKab
-    if aliceKab == bobKab:
-        show("Shared key", Kab)
-        print("")
-
-        if not bbs.is_coprime(Kab, q):
-            print("WARNING: Global parameter q is not coprime to shared key")
-        if not bbs.is_coprime(Kab, p):
-            print("WARNING: Global parameter p is not coprime to shared key")
-
-    log("Initializing %d-bit Blum Blum Shub with shared key as seed ... " % bits)
-    csprng = create_csprng(Kab, bits)
+    log("Generating %d-bit keypair ... " % bits)
+    privkey, pubkey = generate_keypair(csprng_key, generator, p, q)
     log("\n")
-    seed = csprng.get_random_bits(bits)
-    show("%d-bit Blum Blum Shub seed for symmetric cipher" % bits, seed)
+
+    print("Our keys")
+    show("privkey", privkey)
+    print("")
+    show("pubkey", pubkey)
     print("")
 
-    print("Reading plaintext from file %r" % opts.filename)
-    plaintext = read_file(opts.filename)
-    print("")
-
-    prior_sha1 = hashlib.sha1(plaintext).hexdigest()
-    print("Plaintext (SHA1 %s):\n%r" % (prior_sha1, plaintext))
-    print("")
-
-    ciphertext = encrypt_with_csprng(q, p, seed, map(ord, plaintext))
-    print("Ciphertext:\n%r" % ciphertext)
-    print("")
-
-    decrypted = decrypt_with_csprng(q, p, seed, ciphertext)
-    post_sha1 = hashlib.sha1(decrypted).hexdigest()
-    print("Decrypted plaintext (SHA1 %s):\n%r" % (post_sha1, decrypted))
-    print("")
-
-    print("Results")
-    if prior_sha1 == post_sha1:
-        print("  SHA1 sums equal")
+    # Exchange keys over TCP/IP
+    net = Connection(q, p, bits, privkey, pubkey, opts.port, opts.remote_host)
+    if net.remote_host is None:
+        net.start_server()
     else:
-        print("  SHA1 sums NOT equal")
-
-    if decrypted == plaintext:
-        print("  Plaintexts are equal")
-    else:
-        print("  Plaintexts are NOT equal")
+        net.start_client()
 
 if __name__ == "__main__":
     opts = parse_args()
